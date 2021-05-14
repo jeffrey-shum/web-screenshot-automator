176dea08cb2f25fc2b936baa816cb94c7a9507e4
jeffrey-shum <jefffshum1@gmail.com>
2021-05-14 14:22:45 +0900

    Initial commit

---
+++ imgkit_example.py
@@ -0,0 +1,4 @@
+import imgkit
+
+imgkit.from_url('https://custom.firstrepublic.com/heritagebook/v1/chapters/chapter-8/', 'out.jpg')
+
---
+++ selenium_script.py
@@ -0,0 +1,36 @@
+import json
+import time
+from selenium import webdriver
+
+urls = []
+with open('urls5.txt', 'r') as f:
+    for line in f:
+        urls.append(line)
+appState = {
+    "recentDestinations": [
+        {
+            "id": "Save as PDF",
+            "origin": "local",
+            "account": ""
+        }
+    ],
+    "selectedDestinationId": "Save as PDF",
+    "version": 2
+}
+
+profile = {'printing.print_preview_sticky_settings.appState': json.dumps(appState),
+           'savefile.default_directory': '/Users/jeffshum/Dev/webscreenshot-frb/webpages/'}
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option('prefs', profile)
+chrome_options.add_argument('--kiosk-printing')
+
+for url in urls:
+    driver = webdriver.Chrome(options=chrome_options)
+    #driver.implicitly_wait(10)
+    driver.get(url)
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    time.sleep(5)
+    #driver.execute_script('document.title="{}";'.format('url'));
+    driver.execute_script('window.print();')
+driver.quit()
