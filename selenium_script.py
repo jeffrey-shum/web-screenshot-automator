2417d4e75d5934a84f547524025a665d90bc0635
jeffrey-shum <jefffshum1@gmail.com>
2021-07-21 14:39:20 -0700

    Update script to parse urls and remove pipes from page titles

--- selenium_script.py
+++ selenium_script.py
@@ -1,11 +1,11 @@
 import time
 from selenium import webdriver
 import json
+import re
 
-urls = []
 with open('urls.txt', 'r') as f:
-    for line in f:
-        urls.append(line)
+    lines = f.read()
+    urls = re.findall('(https://.*?.html)', lines) # https://stackoverflow.com/a/50977427/13767695
 appState = {
     "recentDestinations": [
         {
@@ -30,14 +30,21 @@ driver = webdriver.Chrome(options=chrome_options)
 def save_screenshot(url):
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
     driver.get(url)
+    #TODO: Click all elements: class="expander__trigger"
+    expanders = driver.find_elements_by_class_name("expander__trigger")
+    for expander in expanders:
+        expander.click()
+    time.sleep(5) # waiting for page to load after clicking expander buttons
     # Ref: https://stackoverflow.com/a/52572919/
     original_size = driver.get_window_size()
     required_width = driver.execute_script('return document.body.parentNode.scrollWidth')
     required_height = driver.execute_script('return document.body.parentNode.scrollHeight')
     driver.set_window_size(required_width, required_height)
     # driver.save_screenshot(path)  # has scrollbar
-    driver.find_element_by_tag_name('body').screenshot(f'./webpage_screenshots/{driver.title}.png')  # avoids scrollbar
+    title = " ".join(driver.title.replace("|",'').split())
+    driver.find_element_by_tag_name('body').screenshot(f'./webpage_screenshots_2021.07.21/{title}.png')  # avoids scrollbar
     driver.set_window_size(original_size['width'], original_size['height'])
+    #TODO: remove pipes from titles then save
 
 if __name__ == '__main__':
     for url in urls:
